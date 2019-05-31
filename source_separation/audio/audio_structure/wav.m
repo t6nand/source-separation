a3337a04c76070df26b0fd402f857b05e016dc21
Tapan Sharma <01tapansharma@gmail.com>
2019-05-31 02:51:06 +0200

    Added num channels property and methods.

--- source_separation/audio/audio_structure/wav.m
+++ source_separation/audio/audio_structure/wav.m
@@ -1,14 +1,41 @@
 classdef wav < audio_interface
     properties 
+        num_channels
     end
     methods
         function obj = wav(file_path)
             obj.audio_file_path = file_path;
-            [obj.sampled_audio, obj.sampling_rate] = audioread(file_path);
+            [obj.sampled_audio_original, obj.sampling_rate] = audioread(file_path);
+            [~, obj.num_channels] = size(obj.sampled_audio_original); 
         end
+        
         function wav_read = get_sampled_audio(obj)
-            wav_read = obj.sampled_audio;
+            wav_read = obj.sampled_audio_original;
         end
+        
+        function mono_channel_audio = get_sampled_audio_mono(obj)
+            if isempty(obj.sampled_audio_mono)
+                if obj.num_channels > 1
+                    mono = double.empty();
+                    for ch = 1:obj.num_channels
+                        if isempty(mono)
+                            mono = obj.sampled_audio_original(:,ch);
+                        else
+                            mono = mono + ...
+                                obj.sampled_audio_original(:,ch);
+                        end
+                    end
+                    mono = mono / obj.num_channels;
+                    mono_channel_audio = mono;
+                else 
+                    mono_channel_audio = get_sampled_audio(obj);
+                    obj.sampled_audio_mono = mono_channel_audio;
+                end
+            else
+                mono_channel_audio = obj.sampled_audio_mono;
+            end
+        end
+        
         function sampling_rate = get_sampling_rate(obj)
             sampling_rate = obj.sampling_rate;
         end
