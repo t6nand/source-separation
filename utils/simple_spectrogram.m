a0cd07929128ab85d80d990e98b0843f1f0b1aee
Tapan Sharma <01tapansharma@gmail.com>
2019-05-31 18:24:55 +0200

    Fixing wrong usage of parameter.

--- utils/simple_spectrogram.m
+++ utils/simple_spectrogram.m
@@ -1,6 +1,6 @@
 function [] = simple_spectrogram(sampled_audio, sampling_rate)
-    window = round(0.05*sampling_rate());
-    overlap = round(0.025*sampling_rate());
+    window = round(0.05*sampling_rate);
+    overlap = round(0.025*sampling_rate);
     % This function plots the simple spectrogram of a sampled audio
     % vector by calculating STFT of audio.
      spectrogram(sampled_audio, window, overlap, [], sampling_rate, 'yaxis');
