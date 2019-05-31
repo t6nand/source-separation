70e95335a9edaa944b4ed61b4ce78a2dbbe7d841
Tapan Sharma <01tapansharma@gmail.com>
2019-05-31 02:50:07 +0200

    Added mono channel audio property

--- source_separation/audio/audio_structure/audio_interface.m
+++ source_separation/audio/audio_structure/audio_interface.m
@@ -1,12 +1,15 @@
 classdef audio_interface
-   properties
+   properties (Access = protected)
       audio_file_path
-      sampled_audio
+      sampled_audio_original
+      sampled_audio_mono
       sampling_rate
    end
    methods (Abstract)
       % This function returns the sampled audio from the raw audio. 
       get_sampled_audio(obj)
+      % This function returns the mono channel sampled audio from the raw audio.
+      get_sampled_audio_mono(obj)
       % This function returns the sampling rate of the audio.
       get_sampling_rate(obj)
    end
