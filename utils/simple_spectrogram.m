98a80079643eeea9bb867e939cc54e95cdbce286
Tapan Sharma <01tapansharma@gmail.com>
2019-05-31 02:54:28 +0200

    Changed spectrogram plot to include window & overlap.

--- utils/simple_spectrogram.m
+++ utils/simple_spectrogram.m
@@ -1,6 +1,8 @@
 function [] = simple_spectrogram(sampled_audio, sampling_rate)
+    window = round(0.05*sampling_rate());
+    overlap = round(0.025*sampling_rate());
     % This function plots the simple spectrogram of a sampled audio
     % vector by calculating STFT of audio.
-     spectrogram(sampled_audio, 512, [], [], sampling_rate, 'yaxis');
+     spectrogram(sampled_audio, window, overlap, [], sampling_rate, 'yaxis');
      title('Audio Spectrogram');
 end
