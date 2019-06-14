cb8e6868c9376b20a518d7d2496c2b410aad9a4d
Tapan Sharma <01tapansharma@gmail.com>
2019-06-14 05:45:24 +0200

    Changed randomize to return audio structure.

--- utils/randomize.m
+++ utils/randomize.m
@@ -1,17 +1,12 @@
 function [s,n]= randomize(a,b)    
-% Enter the directory of speech signal in variable a and of noise signal in
-% b
-    speech=audioDatastore(a,'IncludeSubfolders',true,...
-        'FileExtensions','.wav'); %Extract the speech dataset
-    noise=audioDatastore(b,'IncludeSubfolders',true,'FileExtensions','.wav');...
-        %Extract the noise dataset
-    count1=numel(speech.Files); %Count the number of files 
+% Enter the audiodatastore of speech signal in variable a and of noise 
+% signal in b
+    count1=numel(a.Files); %Count the number of files 
     i1=randi([1,count1],1); %Generate a random integer between 1 and the number...
                           % of files
-    count2=numel(noise.Files); %Count the number of files 
+    count2=numel(b.Files); %Count the number of files 
     i2=randi([1,count2],1); %Generate a random integer between 1 and the number...
                           % of files
-
-    s = wav(speech.Files{i1});
-    n = wav(noise.Files{i2});
+    s = wav(a.Files{i1}, [], []);
+    n = wav(b.Files{i2}, [], []);
 end
