86fd3e20a4dca51cb9116866c97a33451ce77272
Tapan Sharma <01tapansharma@gmail.com>
2019-06-14 05:49:31 +0200

    Changed to return audio structure collection.

--- utils/gen_train_data.m
+++ utils/gen_train_data.m
@@ -1,21 +1,23 @@
 function [train_data_struct, val_data_struct] = gen_train_data(clean_path, noise_path, samples, val_perc, val_snr, train_snr, save)
     % GEN_TRAIN_DATA: This method generates training and validation data;
-    clean_ads = audioDatastore(clean_path);
-    noise_ads = audioDatastore(noise_path);
-    validation_samples = round(val_perc * samples);
-    train_data = double.empty();
-    val_data = double.empty();
+    validation_samples = floor(val_perc * samples);
+    train_data_struct = double.empty();
+    val_data_struct = double.empty();
+    skip_add_train = false;
+    speech=audioDatastore(clean_path,'IncludeSubfolders',true,...
+        'FileExtensions','.wav'); %Extract the speech dataset
+    noise=audioDatastore(noise_path,'IncludeSubfolders',true,'FileExtensions','.wav');...
+        %Extract the noise dataset
     for i=1:samples
-        skip_add_train = false;
         if mod(i, validation_samples) == 0 && validation_samples ~=0
-            [mix_data, clean_data, noise_data] = gen_noisy_audio(clean_path, noise_path, val_snr);
-            val_data = [val_data; mix_data clean_data noise_data];
+            [mix_data_struct, clean_data_struct, noise_data_struct] = gen_noisy_audio(speech, noise, val_snr);
+            val_data_struct = [val_data_struct; mix_data_struct clean_data_struct noise_data_struct];
             validation_samples = validation_samples - 1;
             skip_add_train = true;
         end
         if ~skip_add_train
-            [mix_data, clean_data, noise_data] = gen_noisy_audio(clean_path, noise_path, train_snr); 
-            train_data = [train_data; mix_data clean_data noise_data];
+            [mix_data_struct, clean_data_struct, noise_data_struct] = gen_noisy_audio(speech, noise, train_snr); 
+            train_data_struct = [train_data_struct; mix_data_struct clean_data_struct noise_data_struct];
         else
             skip_add_train = false;
         end
