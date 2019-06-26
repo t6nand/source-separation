b88ab2095e4588552b4912ceb569cf1885df9644
Tapan Sharma <01tapansharma@gmail.com>
2019-06-26 18:41:08 +0200

    Upated struct2features to preserve phase of STFT in mask.

--- utils/struct2features.m
+++ utils/struct2features.m
@@ -5,28 +5,31 @@ function [normalised_feature, mask_seq] = struct2features(mixture_struct, ...
         disp('ERROR!!! size of both clean and noise data arrays are not same');
         return;
     end
-    normalised_feature = zeros(10000,500);
+    normalised_feature = zeros(100,500);
     mask_seq = double.empty();
-    new_row = 1;
-    col_idx = 0;
+    new_col = 1;
+    row_idx = 0;
     for i=1:length(mixture_struct)
         a_f = audio_features(mixture_struct(i,1));
         c_f = audio_features(clean_struct(i,1));
         n_f = audio_features(noise_struct(i,1));
-        [c_power, ~] = c_f.get_stft(clean_struct(i,1));
-        [n_power, ~] = n_f.get_stft(noise_struct(i,1));
-        norm_feats = a_f.get_normalised_features(mixture_struct(i,1));
-        c_power = abs(c_power);
-        c_power = c_power.^2;
-        n_power = abs(n_power);
-        n_power = n_power.^2;
+        try
+            c_power = c_f.get_stft(clean_struct(i,1));
+            n_power = n_f.get_stft(noise_struct(i,1));
+            norm_feats = a_f.get_normalised_features(mixture_struct(i,1));
+        catch
+            continue;
+         end
         mask = (c_power ./ (c_power + n_power + eps));
         mask = mask.^0.5;
-        mask_seq = [mask_seq mask];
-        cur_row = size(norm_feats,1);
-        col_idx = size(norm_feats,2);
-        normalised_feature(new_row:(new_row-1)+cur_row, 1:col_idx) = norm_feats;
-        new_row = (new_row - 1) + cur_row + 1;
+        mask2d = [real(mask) imag(mask)];
+        mask_seq = [mask_seq mask2d];
+        norm_feats = norm_feats';
+        norm_feats_padded = [norm_feats zeros(size(norm_feats,1), size(mask2d,2) - size(norm_feats,2))];
+        cur_col = size(norm_feats_padded,2);
+        row_idx = size(norm_feats_padded,1);
+        normalised_feature(1:row_idx, new_col:(new_col-1)+cur_col) = norm_feats_padded;
+        new_col = (new_col - 1) + cur_col + 1;
     end
-    normalised_feature = normalised_feature(1:new_row-1,1:col_idx);
+    normalised_feature = normalised_feature(1:row_idx, 1:new_col-1);
 end
