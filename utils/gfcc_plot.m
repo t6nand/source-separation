43466478438bdfae4a82a9444b92dd16d056524e
Tapan Sharma <01tapansharma@gmail.com>
2019-07-19 19:43:09 +0200

    added plot legends.

--- utils/gfcc_plot.m
+++ utils/gfcc_plot.m
@@ -4,5 +4,6 @@ t = loc./fs;
 plot(t, coeff);
 xlabel('Time (s)')
 title('Gammatone Cepstral Coefficients')
+legend('logE','1','2','3','4','5','6','7','8','9','10','11','12','13','Location','northeastoutside');
 end
 
